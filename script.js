245038d5ec0e8142de3beef0b85b1a892e7a48e1
Protyasha roy <protyasharoy369@gmail.com>
2024-02-11 21:01:42 +0600

    added remove button on click

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ document.getElementById('noButton').addEventListener('mouseenter', function() {
     const newY = Math.random() * (viewportHeight - buttonRect.height);
     this.style.transform = 'translate(' + newX + 'px, ' + newY + 'px)';
   });
-
+  document.getElementById('noButton').addEventListener('onclick', function() {
+    const buttonRect = this.getBoundingClientRect();
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    const newX = Math.random() * (viewportWidth - buttonRect.width);
+    const newY = Math.random() * (viewportHeight - buttonRect.height);
+    this.style.transform = 'translate(' + newX + 'px, ' + newY + 'px)';
+  });
   document.getElementById('yesButton').addEventListener('click', function() {
     const container = document.querySelector('.container');
     container.innerHTML = '<img src="cheering.gif" alt="Funny GIF">';
